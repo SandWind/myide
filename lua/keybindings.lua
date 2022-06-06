9a211285853fe0b28ad05e7aedc8604158b7059d
SandWind <victorp_460@163.com>
2022-06-06 17:04:07 +0800

    添加ctrl+s 保存 ctrl+q 保存退出  按键映射

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -30,6 +30,8 @@ map("c", "<C-k>", "<C-p>", { noremap = false })
 
 map("n", "<leader>w", ":w<CR>", opt)
 map("n", "<leader>wq", ":wqa!<CR>", opt)
+map("n","<C-s>","w<CR>",opt) ---Ctrl+s保存文件
+map("n","<C-q>",":wqa!<CR>",opt)
 
 -- fix :set wrap
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
